48b660b11f2b416de4b71f365125d67a3e3a3105
Dustin <p4569zz@gmail.com>
2020-07-12 15:18:57 +0900

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Dustin on 2020/07/12.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
