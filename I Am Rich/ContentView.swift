53826c89ebb01616b2144db02530642741596c10
Dustin <p4569zz@gmail.com>
2020-07-12 15:26:00 +0900

    first commit

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("I Am Rich")
+            .font(.title)
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue:"iPhone Xs Max"))
     }
 }
